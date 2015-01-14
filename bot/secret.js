f05da8f34b3e2faccbd733f82a8c4c62cb618f7a
DonPage <don.page.work@gmail.com>
2015-01-14 02:15:05 -0500

    changed ignore files

--- bot/secret.js
+++ bot/secret.js
@@ -3,7 +3,7 @@ var twitchbot = require('./twitchbot.js');
 var bot = twitchbot.create({
   name: 'XXXX',
   channel: 'XXXX',
-  password: 'oauth:XXXX'
+  password: 'XXXXXXXX'
 });
 
 module.exports = bot;
